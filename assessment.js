129a22cfc1586a9050545ffefa3ec33de4e27059
tails1434 <49484039+tails1434@users.noreply.github.com>
2019-04-11 20:59:38 +0900

    #1 に対する対応を行いました

--- assessment.js
+++ assessment.js
@@ -4,25 +4,29 @@
     const assessmentButton = document.getElementById('assessment');
     const resultDivided = document.getElementById('result-area');
     const tweetDivided = document.getElementById('tweet-area');
-
-    /**
-    * 指定した要素の子どもを全て除去する
-    * @param {HTMLElement} element HTMLの要素
-    */
+    
     function removeAllChildren(element) {
-        while (element.firstChild) { // 子どもの要素があるかぎり削除
+        while (element.firstChild) {
             element.removeChild(element.firstChild);
+        }
+    }
+    
+    userNameInput.onkeydown = (event) => {
+        if (event.key === 'Enter'){
+            assessmentButton.onclick();
         }
     }
 
     assessmentButton.onclick = () => {
         const userName = userNameInput.value;
-        if (userName.length === 0) { // 名前が空の時は処理を終了する
+        if (userName.length === 0) {
             return;
         }
 
         // 診断結果表示エリアの作成
         removeAllChildren(resultDivided);
+        
+
         const header = document.createElement('h3');
         header.innerText = '診断結果';
         resultDivided.appendChild(header);
@@ -32,25 +36,21 @@
         paragraph.innerText = result;
         resultDivided.appendChild(paragraph);
 
-        // ツイートエリアの作成
+        // TODO ツイートエリアの作成
         removeAllChildren(tweetDivided);
         const anchor = document.createElement('a');
-        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
-        + encodeURIComponent(result);
+        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
+            + encodeURIComponent('あなたのいいところ')
+            + '&ref_src=twsrc%5Etfw';
         anchor.setAttribute('href', hrefValue);
         anchor.className = 'twitter-hashtag-button';
-        anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
+        anchor.setAttribute('data-text', result);
+        anchor.innerText = 'Tweet #あなたのいいところ';
         tweetDivided.appendChild(anchor);
 
         twttr.widgets.load();
-    };
 
-    userNameInput.onkeydown = (event) => {
-        if (event.keyCode === 13) {
-            assessmentButton.onclick();
-        }
     };
-
     const answers = [
         '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
         '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
@@ -67,14 +67,14 @@
         '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
         '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
         '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-        '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
+        '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
+        '{userName}のいいところは優しさです。{userName}の優しい雰囲気や立ち振る舞いに多くの人が癒やされています。'
     ];
-
     /**
-    * 名前の文字列を渡すと診断結果を返す関数
-    * @param {string} userName ユーザーの名前
-    * @return {string} 診断結果
-    */
+     * 名前の文字列を渡すと診断結果を返す関数
+     * @param {string} userName ユーザの名前
+     * @return {string} 診断結果
+     */
     function assessment(userName) {
         // 全文字のコード番号を取得してそれを足し合わせる
         let sumOfcharCode = 0;
@@ -82,21 +82,21 @@
             sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
         }
 
-        // 文字のコード番号の合計を回答の数で割って添字の数値を求める
+        // 文字のコード番号の合計を解凍の数で割って添え字の数値を求める
         const index = sumOfcharCode % answers.length;
         let result = answers[index];
 
-        result = result.replace(/{userName}/g, userName);
+        result = result.replace(/\{userName}/g, userName);
+
         return result;
     }
-
-    // テストコード
     console.assert(
         assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
         '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
     );
     console.assert(
         assessment('太郎') === assessment('太郎'),
-        '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+        '同一の名前が入力されても同じ診断結果が返っていません。'
     );
+
 })();
